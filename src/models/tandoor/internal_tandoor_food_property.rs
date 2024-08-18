888915b45054a8c89ddfc8447d9e6ea38734f5ac
Tim Kolloch <kolloch.tim@web.de>
2024-08-18 17:54:01 +0200

    feature!(main): Added override mode.
    BREAKING CHANGE: The program does not override data by default anymore. The user must now run the program with --override set so that currently present properties are overridden

--- src/models/tandoor/internal_tandoor_food_property.rs
+++ src/models/tandoor/internal_tandoor_food_property.rs
@@ -2,10 +2,24 @@
 use serde::{Serialize, Deserialize};
 
 use crate::models::tandoor::internal_tandoor_property::InternalTandoorProperty;
+use crate::models::usda::usda_nutrient::USDANutrient;
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct InternalTandoorFoodProperty {
     /// How much of the property is in that food.
     pub property_amount: Option<f32>,
     /// Definition of the property that is in that food.
     pub property_type: InternalTandoorProperty
+}
+
+impl From<&USDANutrient> for InternalTandoorFoodProperty{
+    fn from(value: &USDANutrient) -> Self {
+        InternalTandoorFoodProperty {
+            property_amount: value.amount,
+            property_type: InternalTandoorProperty {
+                name: value.nutrient_information.name.to_string(),
+                fdc_id: value.nutrient_information.id
+            }
+        }
+    }
 }
